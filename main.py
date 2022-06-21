1917ed771ca4ee8bfb24680dd58492a339b92b08
Franco Lopez Paviolo <francomlopez@mi.unc.edu.ar>
2022-06-21 17:12:35 -0300

    added pymakr.conf

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-import pycom
-pycom.heartbeat(False)
-pycom.rgbled(0x330033)
+from network import WLAN
+wlan = WLAN()
+
+wlan.init(mode=WLAN.AP, ssid='hello world')
+#use the line below to apply a password
+#wlan.init(ssid="hi", auth=(WLAN.WPA2, "eightletters"))
+print(wlan.ifconfig(id=1)) #id =1 signifies the AP interface
